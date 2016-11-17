246efd37f0d4702678eb3570c707e8f57eda5395
afghl <airport.0930@gmail.com>
2016-11-17 22:35:39 +0800

    initial commit

---
+++ src/main/java/com/sogrp/ForumJavaApplication.java
@@ -0,0 +1,12 @@
+package com.sogrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumJavaApplication.class, args);
+	}
+}
